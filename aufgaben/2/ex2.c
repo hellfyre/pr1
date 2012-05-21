c12ee7c3b05dbae2735dfd9a87c689ca6b654b58
Matthias Uschok <matthias@uschok.de>
2012-05-21 17:29:55 +0200

    Exercise 2a

--- aufgaben/2/ex2.c
+++ aufgaben/2/ex2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <mpi.h>
 
 #include <measure_processtime.h>
@@ -8,6 +8,14 @@ int main(int argc, char **argv) {
   static int local_rank = -1;
   static long time = 0;
 
+  int i, curr_size;
+  int max_elements = 1000 * 100;
+  int *foobar = malloc( sizeof(int) * max_elements);
+  for (i=0; i<max_elements; i++) {
+    foobar[i] = 42;
+  }
+  MPI_Status somestatus;
+
   measure_init();
 
   MPI_Init(&argc, &argv);
@@ -15,16 +23,24 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &local_rank);
 
     if ( local_rank == 0 ) {
-      usleep(100000); // sleep for 100ms to make sure the MPI_Receive on the other side is ready
-      measure_start();
-      MPI_Send();
-      MPI_Receive();
-      time = measure_end();
-      printf("Process %d received %s.\n", local_rank);
+      for (i=1; i<=25; i++) {
+        curr_size = 1000 * i;
+        //printf("i: %d curr_size: %d\n", i, curr_size);
+        MPI_Barrier(MPI_COMM_WORLD);
+        measure_start();
+        MPI_Send(foobar, curr_size, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(foobar, curr_size, MPI_INT, 1, 0, MPI_COMM_WORLD, &somestatus);
+        time = measure_end();
+        printf("%d %ld\n", (curr_size*4)/1000, time);
+      }
     }
     else {
-      MPI_Receive();
-      MPI_Send();
+      for (i=1; i<=25; i++) {
+        curr_size = 1000 * i;
+        MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Recv(foobar, curr_size, MPI_INT, 0, 0, MPI_COMM_WORLD, &somestatus);
+        MPI_Send(foobar, curr_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+      }
     }
 
   MPI_Finalize();
