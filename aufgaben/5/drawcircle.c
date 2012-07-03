70844af303cf156681d06e1a04089481b66b6628
Matthias Uschok <dev@uschok.de>
2012-07-03 18:17:25 +0200

    Drawing circles and saving them as BMP works

--- aufgaben/5/drawcircle.c
+++ aufgaben/5/drawcircle.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <math.h>
 
-#define WIDTH 157
-#define HEIGHT 64
+#include "bitmap.c"
+
+#define WIDTH 500
+#define HEIGHT 500
 
 typedef unsigned char byte;
+typedef struct {
+  byte blue;
+  byte green;
+  byte red;
+} color;
 
-byte image[WIDTH*HEIGHT];
+color image[WIDTH*HEIGHT];
 
 void swap (int *a, int *b) {
   int tmp = *a;
@@ -16,11 +22,11 @@ void swap (int *a, int *b) {
   *b = tmp;
 }
 
-void setPixel(int x, int y) {
-  image[WIDTH * y + x] = 0xff;
+void setPixel(int x, int y, color pcolor) {
+  image[WIDTH * y + x] = pcolor;
 }
 
-void drawLine(int x0, int y0, int x1, int y1) {
+void drawLine(int x0, int y0, int x1, int y1, color lcolor) {
   int steep = abs(y1 - y0) > abs(x1 - x0);
 
   if (steep) {
@@ -42,8 +48,8 @@ void drawLine(int x0, int y0, int x1, int y1) {
   else ystep = -1;
 
   for (int x=x0; x<=x1; x++) {
-    if (steep) setPixel(y,x);
-    else setPixel(x,y);
+    if (steep) setPixel(y, x, lcolor);
+    else setPixel(x, y, lcolor);
 
     error = error - deltay;
     if ( error < 0) {
@@ -54,17 +60,17 @@ void drawLine(int x0, int y0, int x1, int y1) {
 
 }
 
-void drawCircle(int x0, int y0, int radius) {
+void drawCircle(int x0, int y0, int radius, color ccolor) {
   int f = 1 - radius;
   int ddF_x = 1;
   int ddF_y = -2 * radius;
   int x = 0;
   int y = radius;
  
-  setPixel(x0, y0 + radius);
-  setPixel(x0, y0 - radius);
-  setPixel(x0 + radius, y0);
-  setPixel(x0 - radius, y0);
+  setPixel(x0, y0 + radius, ccolor);
+  setPixel(x0, y0 - radius, ccolor);
+  setPixel(x0 + radius, y0, ccolor);
+  setPixel(x0 - radius, y0, ccolor);
  
   while(x < y) {
     // ddF_x == 2 * x + 1;
@@ -78,28 +84,28 @@ void drawCircle(int x0, int y0, int radius) {
     x++;
     ddF_x += 2;
     f += ddF_x;    
-    setPixel(x0 + x, y0 + y);
-    setPixel(x0 - x, y0 + y);
-    setPixel(x0 + x, y0 - y);
-    setPixel(x0 - x, y0 - y);
-    setPixel(x0 + y, y0 + x);
-    setPixel(x0 - y, y0 + x);
-    setPixel(x0 + y, y0 - x);
-    setPixel(x0 - y, y0 - x);
+    setPixel(x0 + x, y0 + y, ccolor);
+    setPixel(x0 - x, y0 + y, ccolor);
+    setPixel(x0 + x, y0 - y, ccolor);
+    setPixel(x0 - x, y0 - y, ccolor);
+    setPixel(x0 + y, y0 + x, ccolor);
+    setPixel(x0 - y, y0 + x, ccolor);
+    setPixel(x0 + y, y0 - x, ccolor);
+    setPixel(x0 - y, y0 - x, ccolor);
   }
 }
 
-void drawCircleFill(int x0, int y0, int radius) {
+void drawCircleFill(int x0, int y0, int radius, color ccolor) {
   int f = 1 - radius;
   int ddF_x = 1;
   int ddF_y = -2 * radius;
   int x = 0;
   int y = radius;
  
-  drawLine(x0, y0, x0, y0 + radius);
-  drawLine(x0, y0, x0, y0 - radius);
-  drawLine(x0, y0, x0 + radius, y0);
-  drawLine(x0, y0, x0 - radius, y0);
+  drawLine(x0, y0, x0, y0 + radius, ccolor);
+  drawLine(x0, y0, x0, y0 - radius, ccolor);
+  drawLine(x0, y0, x0 + radius, y0, ccolor);
+  drawLine(x0, y0, x0 - radius, y0, ccolor);
  
   while(x < y) {
     // ddF_x == 2 * x + 1;
@@ -113,16 +119,22 @@ void drawCircleFill(int x0, int y0, int radius) {
     x++;
     ddF_x += 2;
     f += ddF_x;    
-    drawLine(x0 - x, y0 + y, x0 + x, y0 + y);
-    drawLine(x0 - x, y0 - y, x0 + x, y0 - y);
-    drawLine(x0 - y, y0 + x, x0 + y, y0 + x);
-    drawLine(x0 - y, y0 - x, x0 + y, y0 - x);
+    drawLine(x0 - x, y0 + y, x0 + x, y0 + y, ccolor);
+    drawLine(x0 - x, y0 - y, x0 + x, y0 - y, ccolor);
+    drawLine(x0 - y, y0 + x, x0 + y, y0 + x, ccolor);
+    drawLine(x0 - y, y0 - x, x0 + y, y0 - x, ccolor);
   }
 }
 
 int main() {
-  drawCircleFill(30, 25, 10);
+  color someColor;
+  someColor.red = 0xab;
+  someColor.green = 0xe1;
+  someColor.blue = 0x20;
 
+  drawCircleFill(250, 200, 50, someColor);
+
+  /*
   for (int row=0; row<HEIGHT; row++) {
     for (int column=0; column<WIDTH; column++) {
       int elem = WIDTH * row + column;
@@ -132,4 +144,9 @@ int main() {
     }
     putchar('\n');
   }
+  */
+
+  saveBMP("circles.bmp", image, 500, 500, 0);
+
+  return 0;
 }
